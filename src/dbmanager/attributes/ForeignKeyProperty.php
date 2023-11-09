34a3ab379b1ab4a53325a2834857c581d2f471d6
Marius Karstedt <marius@gastrovia.app>
2023-11-09 16:23:19 +0100

    switch parameters

--- src/dbmanager/attributes/ForeignKeyProperty.php
+++ src/dbmanager/attributes/ForeignKeyProperty.php
@@ -14,25 +14,25 @@ class ForeignKeyProperty
 {
 
     /**
-     * @param string|null $references_column
      * @param string|null $references_table
+     * @param string|null $references_column
      * @param string|null $on_delete
      * @param string|null $on_update
      */
-    public function __construct(private readonly ?string $references_column = null, private readonly ?string $references_table = null, #[ExpectedValues([null, 'CASCADE'])] private readonly ?string $on_delete = null, private readonly ?string $on_update = null){}
-
-    /**
-     * @return string|null
-     */
-    public function getReferencesColumn(): ?string{
-        return $this->references_column;
-    }
+    public function __construct(private readonly ?string $references_table = null, private readonly ?string $references_column = 'id', #[ExpectedValues([null, 'CASCADE'])] private readonly ?string $on_delete = null, #[ExpectedValues([null, 'CASCADE'])] private readonly ?string $on_update = null){}
 
     /**
      * @return string|null
      */
     public function getReferencesTable(): ?string{
         return $this->references_table;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getReferencesColumn(): ?string{
+        return $this->references_column;
     }
 
     /**
